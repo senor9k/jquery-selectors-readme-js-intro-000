5cc19e26c062e74f825615cb74a64a74d4712352
legacy-collaborator-7770 <johnvasiliadis@me.com>
2019-01-15 17:47:27 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,12 +1,8 @@
 // declare your functions here...
 function paragraphSelector(){
-  $( document ).ready( function () {
     $('p').text()
-  });
 }
 
 function lastImageSelector(){
-  $( document ).ready( function () {
     $('img:last')
-  });
 }
